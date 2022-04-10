2b301f909bd3f7faf7b52f9221b44fdf6665f17f
siavava <amittaijoel@outlook.com>
2022-04-10 18:29:17 -0400

    gcd et mod

---
+++ crypt/ciphers/app/AbstractAlgebra.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+module AbstractAlgebra (
+    addN
+  , mulN
+  , subN
+  , divN
+  , invN
+  , gcd
+  , gcdL
+  , prompt
+  , run
+) where
+
+import Prelude hiding ( gcd )
+
+mulN :: Integral a => a -> a -> a -> a
+mulN n a b = (a * b) `mod` n
+
+addN :: Integral a => a -> a -> a -> a
+addN n a b = (a + b) `mod` n
+
+subN :: Integral a => a -> a -> a -> a
+subN n a b = (a - b) `mod` n
+
+
+gcd :: Integer -> Integer -> Integer
+gcd = euclideanA
+
+gcdL :: Integer -> Integer -> IO Integer
+gcdL = euclideanL
+  where
+    euclideanL :: Integer -> Integer -> IO Integer
+    euclideanL a 0 = putStrLn ("gcd " ++ show a ++ " 0: " ++ show a) >> return a
+    euclideanL 0 b = putStrLn ("gcd 0 " ++ show b ++ ": " ++ show b) >> return b
+    euclideanL a b
+        | a == b  = do
+          putStrLn ("gcd " ++ show a ++ " " ++ show b ++ ": " ++ show b)
+          return a
+        | a > b   = do
+          putStrLn ("gcd " ++ show a ++ " " ++ show b ++ "...")
+          euclideanL b (a `mod` b)
+        | otherwise = do
+          putStrLn ("gcd " ++ show a ++ " " ++ show b ++ "...")
+          euclideanL a (b `mod` a)
+
+
+euclideanA :: Integral a => a -> a -> a
+euclideanA a 0 = a
+euclideanA 0 b = b
+euclideanA a b
+  | a == b  = a
+  | a > b   = euclideanA b (a `mod` b)
+  | otherwise = euclideanA a (b `mod` a)
+
+divN :: Integral a => a -> a -> a -> a
+divN n a b = (a * invN n b) `mod` n
+
+invN :: Integral p => p -> p -> p
+invN _ 0 = 0
+invN _ 1 = 1
+invN n a = iter n a 1
+  where
+    mul = mulN n
+    iter n a b
+      | a > n || b > n = 0
+      | a `mul` b == 1 = b
+      | otherwise = iter n a $ b + 1
+
+prompt :: IO ()
+prompt = do
+  putStrLn "Please select an action:"
+  putStrLn "1. Add"
+  putStrLn "2. Subtract"
+  putStrLn "3. Multiply"
+  putStrLn "4. Divide"
+  putStrLn "5. Inverse"
+  putStrLn "6. GCD"
+  putStrLn "7. GCD (Logging)"
+  return ()
+
+run :: Integer -> Integer -> Integer -> Integer -> IO Integer
+run action base a b = case action of
+  1 -> return $ addN base a b
+  2 -> return $ subN base a b
+  3 -> return $ mulN base a b
+  4 -> return $ divN base a b
+  5 -> return $ invN base a
+  6 -> return $ gcd a b
+  7 -> gcdL a b
+  _ -> return 0
--- crypt/ciphers/app/Main.hs
+++ crypt/ciphers/app/Main.hs
@@ -3,7 +3,10 @@ module Main where
 import qualified ShiftCipher as Shift
 import qualified VigenereCipher as Vigenere
 import qualified SubstitutionCipher as Substitution
+import qualified AbstractAlgebra as Algebra
+import System.IO (stdout, hFlush)
 import Control.Monad (void)
+import Control.Monad.Cont (join)
 
 main :: IO ()
 main = do
@@ -31,7 +34,10 @@ main = do
       -- message <- readFile "./data/TEXT2"
       message <- getMessage
       -- key <- getKey
-      let key = 7876565434321123434565678788787656543432112343456567878878765654433211234
+      let key = 0o435237014536765252023174645673653671333323451410336736546663323554124527651421542
+      let key' = 7876565434321123434565678788787656543432112343456567878878765654433211234
+      let key2'' = 0x4753E0CAF7D554133E69777ABDCB6DA7298437BBD66D9B4ED854ABEA62362
+      print $ key == key'
       results <- Vigenere.decrypt key message
       putStrLn $ "\nresults: \n\t" ++ results
       main
@@ -41,7 +47,16 @@ main = do
       plaintext <- Substitution.bruteforce message
       putStrLn $ "original: \n\t" ++ message
       putStrLn $ "\nplaintext: \n\t" ++ plaintext
-    6 -> return ()
+      main
+    6 -> do
+      Algebra.prompt
+      action <- getAction
+      base <- getBase
+      b <- getNumber
+      c <- getNumber
+      Algebra.run action base b c >>= print
+      main
+    7 -> return ()
     _ -> do
       putStrLn "Invalid mode"
       main
@@ -54,7 +69,8 @@ prompt = do
   putStrLn "3. Run brute-force attack..."
   putStrLn "4. Vigenere cipher decryption..."
   putStrLn "5. Decrypt using substitution ciphers..."
-  putStrLn "6. Exit."
+  putStrLn "6. Run algebraic solver..."
+  putStrLn "7. Exit."
 getMessage :: IO String
 getMessage = do
   putStrLn "Enter text / message:"
@@ -62,5 +78,20 @@ getMessage = do
 
 getKey :: IO Int
 getKey = do
-  putStrLn "Enter a key:"
+  putStr "Enter a key: " >> hFlush stdout
   readLn :: IO Int
+
+getAction :: IO Integer
+getAction = do
+  putStr "action: " >> hFlush stdout
+  readLn :: IO Integer
+
+getBase :: IO Integer
+getBase = do
+  putStr "base: " >> hFlush stdout
+  readLn :: IO Integer
+
+getNumber :: IO Integer
+getNumber = do
+  putStr "number: " >> hFlush stdout
+  readLn :: IO Integer
