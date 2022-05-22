fde947e7c859cf29b78b1281a9ea8d90fea5fcaa
siavava <amittaijoel@outlook.com>
2022-05-22 03:54:42 -0400

    backup

--- zeta/data.backup/metadata/index.py
+++ zeta/data.backup/metadata/index.py
@@ -11,6 +11,7 @@ __version__ = "0.0.1"
 def count_words():
   """
     This is a simple script to count the number of words in this directory.
+    
     It loops over all the lines in `.all` and counts the occurrence of each word,
     then sums them up. 
   """
@@ -31,21 +32,24 @@ def count_words():
 def index_pages():
   """
     Generate a friendly index of the pages.
+
     We create a csv and a tsv (in case one proves more convenient than the other).
   """
   docID = 0
 
-  with open("index.csv", "w") as csv, open("index.tsv", "w") as tsv:
+  with open("index.csv", "w") as csv, open("index.tsv", "w") as tsv, open("urls", "w") as urls:
     while True:
       try:
-        with open(f"../{docID}", "r") as doc:
+        with open(f"../log/{docID}", "r") as doc:
           title = doc.readline().strip()
           year = doc.readline().strip()
           url = doc.readline().strip()
+          doc.close()
+
           print(f"Indexing: {docID}")
           csv.write(f"{docID},{year},{title},{url}\n")
           tsv.write(f"{docID}\t{year}\t{title}\t{url}\n")
-          doc.close()
+          urls.write(f"{url}\n")
           docID += 1
       except:
         break
